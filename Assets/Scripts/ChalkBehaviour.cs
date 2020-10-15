d8db2f3205a8cf7ecbaf34c1ebbce5e503bb4c56
Damian Alexandru <damian.alexandru99@gmail.com>
2020-10-15 14:40:52 +0300

    restructured mostly

---
+++ Assets/Scripts/ChalkBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChalkBehaviour : MonoBehaviour
+{
+    public LineRenderer trace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //create a new Trace prefab if chalk is used.
+        //while chalk is used, add x, y, z points to the newly created Trace prefab 
+        int k = 0;
+
+
+        LineRenderer newTrace = Instantiate(trace, transform.position, transform.rotation);
+        newTrace.positionCount++;
+        newTrace.SetPosition(k, transform.position);
+        k++;
+    }
+}
